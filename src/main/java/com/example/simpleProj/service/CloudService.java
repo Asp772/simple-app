8729967d40d9d8ac6eca97107d2fbbf5961caa60
Siarhei Kamarou <skamarou@ibagroup.eu>
2018-07-11 14:59:40 +0300

    s3bucket  uploading method

--- src/main/java/com/example/simpleProj/service/CloudService.java
+++ src/main/java/com/example/simpleProj/service/CloudService.java
@@ -1,12 +1,17 @@
 package com.example.simpleProj.service;
 
+import com.example.simpleProj.exception.CloudServiceUploadingException;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
+import java.util.List;
 
 /**
  * Created by Kamarou_S on 10.07.2018.
  */
 public interface CloudService {
-    String uploadFile(File file);
+    String uploadFile(MultipartFile file) throws CloudServiceUploadingException;
 
-    String testMethod();
+    List<String> getAllFiles();
+
 }
--- src/main/java/com/example/simpleProj/service/impl/AmazonS3ServiceImpl.java
+++ src/main/java/com/example/simpleProj/service/impl/AmazonS3ServiceImpl.java
@@ -2,17 +2,28 @@ package com.example.simpleProj.service.impl;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
+import com.example.simpleProj.exception.CloudServiceUploadingException;
 import com.example.simpleProj.service.CloudService;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Kamarou_S on 10.07.2018.
@@ -20,72 +31,85 @@ import java.io.File;
 
 @Service
 public class AmazonS3ServiceImpl implements CloudService {
-    /*@Autowired
-    private Environment environment;
-    private String AWSaccessKey;
-    private String AWSSecretKey;
-    private String AWSBucketName;
-    private AWSCredentials Credentials;
-    private AmazonS3 s3Client;
 
-    private final Environment environment1;
+    private final Environment environment;
+    private final AWSCredentials credentials;
+    private final AmazonS3 s3Client;
 
     @Autowired
     public AmazonS3ServiceImpl(Environment environment) {
-        this.environment1 = environment;
-        System.out.println(this.getAWSaccessKey());
+        this.environment = environment;
+        this.credentials = new BasicAWSCredentials(this.getAccessKey(), this.getSecretKey());
+        this.s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(this.getBucketRegion())
+                .withCredentials(new AWSStaticCredentialsProvider(this.credentials)).build();
     }
 
-    public String getAWSaccessKey() {
-        return environment.getProperty("AWSAccessKeyId");
+    private String getAccessKey() {
+        return environment.getProperty("aws.key.access");
     }
 
-
-    public String getAWSSecretKey() {
-        return environment.getProperty("AWSSecretKey");
+    private String getSecretKey() {
+        return environment.getProperty("aws.key.secret");
     }
 
-    public String getAWSBucketName() {
-        return environment.getProperty("AWSBucketName");
+    private String getBucketName() {
+        return environment.getProperty("aws.bucket.name");
     }
 
-    public String getAWSUrl() {
-        return environment.getProperty("AWSUrl");
-    }*/
-    /*
+    private String getBucketUrl() {
+        return environment.getProperty("aws.bucket.url");
+    }
 
-    public static String md5Apache(String st) {
+    private String getBucketRegion() {
+        return environment.getProperty("aws.bucket.region");
+    }
+
+    private static String md5Apache(String st) {
         String md5Hex = DigestUtils.md5Hex(st);
-
         return md5Hex;
     }
 
-    public String putObject(File file, String fileName) throws AmazonServiceException {
-        PutObjectResult result = null;
-
-     *//*   AWSaccessKey = env.getProperty("AWSAccessKeyId");
-        AWSSecretKey = env.getProperty("AWSSecretKey");
-        AWSBucketName = env.getProperty("AWSBucketName");*//*
-        //  Credentials = new BasicAWSCredentials(AWSaccessKey, AWSSecretKey);
-        Credentials = new BasicAWSCredentials(getAWSaccessKey(), getAWSSecretKey());
-        s3Client = new AmazonS3Client(Credentials);
-        String resultURL= md5Apache(fileName);
-        result = s3Client.putObject(getAWSBucketName(), resultURL, (File) file);
-        //  result = s3Client.putObject(AWSBucketName, fileName, (File) file);
-        return resultURL;
-    }
-
-*/
-
-    @Override
-    public String uploadFile(File file) {
-        return null;
+    private static File convert(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        file.createNewFile();
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return file;
     }
 
     @Override
-    public String testMethod() {
-        System.out.println();
-        System.out.println("1");
+    public String uploadFile(MultipartFile file) throws CloudServiceUploadingException {
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
+            String fileName = file.getOriginalFilename();
+            String folder = md5Apache(dateFormat.format(date));
+            File receivedFile = convert(file);
+            PutObjectResult result = s3Client.putObject(this.getBucketName() + '/' + folder, fileName, receivedFile);
+            receivedFile.delete();
+            s3Client.setObjectAcl(this.getBucketName(), folder + '/' + fileName, CannedAccessControlList.PublicRead);
+            String resultURL = this.getBucketUrl() + this.getBucketName() + '/' + folder + '/' + fileName;
+            return resultURL;
+        } catch (AmazonServiceException e) {
+            throw new CloudServiceUploadingException(e.getMessage());
+        } catch (IOException e) {
+            throw new CloudServiceUploadingException(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> getAllFiles() {
+        ListObjectsRequest req = new ListObjectsRequest().withBucketName(this.getBucketName());
+        ObjectListing listing = s3Client.listObjects(req);
+        for (String commonPrefix : listing.getCommonPrefixes()) {
+            System.out.println(commonPrefix);
+        }
+        for (S3ObjectSummary summary : listing.getObjectSummaries()) {
+            System.out.println(summary.toString());
+        }
         return null;
     }
+
 }
